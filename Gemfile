0aa2fc25da28e76aad2f5a0a69e1a26456f4ebc3
AndreyChim <chimitcyrenov@mail.ru>
2025-02-14 15:28:12 +0800

    Add tailwind.css

--- Gemfile
+++ Gemfile
@@ -12,5 +12,8 @@ gem "propshaft"
 # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
 gem "rubocop-rails-omakase", require: false
 
+gem "foreman"
+gem "tailwindcss-rails", "~> 4.0"
+
 # Start debugger with binding.b [https://github.com/ruby/debug]
 # gem "debug", ">= 1.0.0"
---
+++ test/dummy/config/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./app/views//*.html.erb",
+      "./app/components//*.html.erb",
+      "./app/helpers//*.rb",
+      "./app/javascript//*.js",
+       './test/dummy/app/views/**/*.{html,erb}'
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
